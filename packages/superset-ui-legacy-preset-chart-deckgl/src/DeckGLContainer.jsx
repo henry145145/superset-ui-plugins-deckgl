f4c1d96e35177276f4a3e8ba85edca08623f80e5
henry145145 <henry@tourmalinelabs.com>
2020-02-13 15:32:00 -0800

    ssssss

--- packages/superset-ui-legacy-preset-chart-deckgl/src/DeckGLContainer.jsx
+++ packages/superset-ui-legacy-preset-chart-deckgl/src/DeckGLContainer.jsx
@@ -90,8 +90,10 @@ export default class DeckGLContainer extends React.Component {
 
   render() {
     const { children, bottomMargin, height, width } = this.props;
-    const { viewport } = this.props;
-    console.log(viewport);
+    const { viewState } = this.state;
+    const initialViewState = this.props.viewport;
+    // const { viewport } = this.props;
+    console.log(viewState);
     const adjustedHeight = height - bottomMargin;
 
     const layers = this.layers();
@@ -104,7 +106,8 @@ export default class DeckGLContainer extends React.Component {
           width={width}
           height={adjustedHeight}
           layers={layers}
-          viewState={viewport}
+          viewState={viewState}
+          initialViewState={initialViewState}
           onViewStateChange={this.onViewStateChange}
         >
           <StaticMap
